cb3aaf58d0970b9328999dd4d4d1f357d33460c6
Ricky de Laveaga <git@rdela.com>
2024-05-01 12:12:09 -0700

    do not show latestPostsCount on index if it is 1 (#27)
    * fix: do not show latestPostsCount on index if it is 1
    
    * v9.2.13-alpha.10

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 let data = {
-	title: "eleventeen v9.2.12-alpha.9",
+	title: "eleventeen v9.2.13-alpha.10",
 	url: "https://eleventeen.blog",
 	language: "en",
 	description: "Rainbow Eleventy blog",
@@ -10,7 +10,7 @@ let data = {
 	},
 	siteimage: "https://o.famebot.com/file/famebot/eleventeen.png",
 	mono: false,
-	eleventeen: "9.2.12-alpha.9",
+	eleventeen: "9.2.13-alpha.10",
 };
 
 export default data;
